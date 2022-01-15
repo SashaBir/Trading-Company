ffaaae0801a6d0f0d6db2d202b33520fe60480c7
SashaBiruykov <sasahkrin.bir@mail.ru>
2022-01-15 22:04:54 +0300

    fix: bugs and pretty scripts

---
+++ TradingCompany/TradingCompany/Project/Extended/ManipulatorExtended.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace TradingCompany.Project.Extended
+{
+    internal static class ManipulatorExtended
+    {
+        internal static void SwitchToOtherPage<T>(this object oldPage, T newPage)
+            where T : Page
+        {
+            if (oldPage == null)
+                throw new Exception("oldPage is null.");
+
+            if (newPage == null)
+                throw new Exception("newPage is null.");
+
+            if ((Frame)oldPage is Frame frame)
+            {
+                frame.Visibility = System.Windows.Visibility.Visible;
+                frame.Content = newPage;
+                return;
+            }
+
+            if ((Page)oldPage is Page page)
+            {
+                page.Visibility = System.Windows.Visibility.Visible;
+                page.Content = newPage;
+                return;
+            }
+        }
+    }
+}
--- TradingCompany/TradingCompany/Project/LoginMenu/Scripts/LoginMenu.cs
+++ TradingCompany/TradingCompany/Project/LoginMenu/Scripts/LoginMenu.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TradingCompany.Project.LoginMenu.Scripts
 {
@@ -38,6 +26,7 @@ namespace TradingCompany.Project.LoginMenu.Scripts
             if (_login.Text == DEFUALT_LOGIN && _password.Text == DEFUALT_Password)
             {
                 // вход в приложение
+                //ManipulatorExtended.SwitchToOtherPage();
             }
         }
     }
